a78bf8577b89db866be158ec244106fcb4b50c8d
unknown <ryancare@protonmail.com>
2022-04-11 16:00:36 -0500

    Added a general layout of components with some placeholder event functionality

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import Form from "./Form";
 
-const AppWrap = styled.div`
-
-`
+const AppWrap = styled.div``;
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          name: "The thing",
+          id: 1,
+          completed: false,
+        },
+        {
+          name: "The next thing",
+          id: 2,
+          completed: false,
+        },
+      ],
+    };
+  }
+
   render() {
-    return (
-      <AppWrap>
-        Todo App
-      </AppWrap>
-    )
+    return <AppWrap>
+      <Form></Form>
+    </AppWrap>;
   }
 }
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,16 +1,21 @@
-import React from 'react'
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import TodoList from "./TodoList";
 
-const FormDiv = styled.div`
-
-`;
+const FormDiv = styled.div``;
 
 export default class Form extends React.Component {
   render() {
     return (
       <FormDiv>
-        Form
+        <h2>Todos:</h2>
+        <TodoList></TodoList>
+        <input type="text" placeholder="Type a thing Todo" />
+        <button 
+         onClick={() => console.log("Submit clicked!")}>Submit</button>
+        <button
+        onClick={() => console.log("Show/Hide Clicked!")}>Completed Button</button>
       </FormDiv>
-    )
+    );
   }
 }
