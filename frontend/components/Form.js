14b6e98ec39691f6d861c4e3848d515516326656
unknown <ryancare@protonmail.com>
2022-04-11 15:05:15 -0500

    Added styled components, created form styled component container.

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import styled from 'styled-components';
+
+const FormDiv = styled.div`
+
+`;
 
 export default class Form extends React.Component {
   render() {
     return (
-      <div>
+      <FormDiv>
         Form
-      </div>
+      </FormDiv>
     )
   }
 }
