07fcd2bebe4cec67dc241cb3cac9915745825cc4
unknown <ryancare@protonmail.com>
2022-04-11 21:18:55 -0500

    Fixed my form (actually had to add one -- I had made a psueod-form with full functionality with the exception of submitting upon hitting enter

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -2,49 +2,52 @@ import React from "react";
 import styled from "styled-components";
 import TodoList from "./TodoList";
 
-const FormDiv = styled.div`
-display:flex;
-flex-flow: column nowrap;
+const FormDiv = styled.form`
+  display: flex;
+  flex-flow: column nowrap;
 `;
 
 export default class Form extends React.Component {
-  
-constructor () {
-  super();
-  this.state = {
-    todoText: ''
+  constructor() {
+    super();
+    this.state = {
+      todoText: "",
+    };
   }
-}
 
-handleChange = e => {
-  this.setState({
-    todoText: e.target.value
-  })
-}
+  handleChange = (e) => {
+    console.log(e);
+    this.setState({
+      todoText: e.target.value,
+    });
+  };
 
-  handleSubmit = e => {
+  handleSubmit = (e) => {
     e.preventDefault();
     this.props.addItem(this.state.todoText);
     this.setState({
-      todoText: ''
-    })
-  }
-
+      todoText: "",
+    });
+  };
 
   render() {
     return (
-      <FormDiv>
+      <FormDiv onSubmit={(e) => this.handleSubmit(e)}>
         <h2>Todos:</h2>
-        <TodoList todos={this.props.todos} toggleItem={this.props.toggleItem}></TodoList>
+        <TodoList
+          todos={this.props.todos}
+          toggleItem={this.props.toggleItem}
+        ></TodoList>
         <input
           type="text"
           placeholder="Type a thing Todo"
           value={this.state.todoText}
-          onChange={e => this.handleChange(e)} />
-        <button 
-         onClick={e => this.handleSubmit(e)}>Submit</button>
-        <button
-        onClick={() => this.props.clearCompleted()}>Clear Completed</button>
+          onChange={(e) => this.handleChange(e)}
+        />
+        <button>Submit</button>
+        <button type="button" onClick={() => this.props.clearCompleted()}>
+          Clear Completed
+        </button>
       </FormDiv>
     );
   }
